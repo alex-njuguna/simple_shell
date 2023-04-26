1a4472d09841679dc5cb93dcea4e6ab53c4cf3f7
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-26 20:43:31 +0300

    mains

--- main.c
+++ main.c
@@ -1,76 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
-
 #include "main.h"
 
-#define MAX_COMMAND_LENGTH 100
-
 /**
- * main - entry point
- * Return: 0
- */
-int main(void)
+* read_input - read input from user
+* Return: input or null
+*/
+char *read_input(void)
 {
-	char command[MAX_COMMAND_LENGTH];
-	size_t length;
-	int status;
+	char *input = malloc(sizeof(char) * MAX_LENGTH);
+	size_t input_size = 0;
 
-	while (1)
+	if (getline(&input, (size_t *) &input_size, stdin) == -1)
 	{
-		printf("$ ");
-		if (fgets(command, MAX_COMMAND_LENGTH, stdin) == NULL)
-		{
-			break;
-		}
-		length = strcspn(command, "\n");
-		if (length == 0)
-		{
-			continue;
-		}
-		command[length] = '\0';
-		status = execute_command(command);
-		if (status == -1)
-		{
-		printf("Error: command '%s' not found\n", command);
-		}
+		free(input);
+		return (NULL);
 	}
-	return (0);
+
+	return (input);
 }
 
 /**
- * execute_command - execute commands collected
- * @command: test par
- * Return: int
- */
-int execute_command(char *command)
+* execute_command - executes commands provided
+* @input: pointer to list of commands
+* @argv: array vector of commands
+* Return: void
+*/
+void execute_command(char *input, char *argv[])
 {
-	pid_t pid = fork();
-	char *args[10];
+pid_t pid = fork();
+int status;
+char *args[4];
 
-	if (pid == -1)
+if (pid == 0)
+{
+args[0] = "/bin/sh";
+args[1] = "-c";
+args[2] = input;
+args[3] = NULL;
+execv(args[0], args);
+fprintf(stderr, "%s: %d: %s: not found\n", argv[0], __LINE__, input);
+exit(EXIT_FAILURE);
+}
+else
+{
+waitpid(pid, &status, 0);
+if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+{
+fprintf(stderr, "%s:%d:%s:%d", argv[0], __LINE__, input, WEXITSTATUS(status));
+}
+else if (WIFSIGNALED(status))
+{
+fprintf(stderr, "%s: %d: %s: %d", argv[0], __LINE__, input, WTERMSIG(status));
+}
+}
+}
+
+/**
+* main - entry point
+* @argc: arg count
+* @argv: arg list
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	char *input;
+	int isatty_mode = isatty(STDIN_FILENO);
+
+	if (isatty_mode)
+		printf("($) ");
+	if (argc > 1)
+		printf("You have listed %d commands.", argc - 1);
+	while (1)
 	{
-		perror("fork");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid == 0)
-	{
-		args[0] = command;
-		args[1] = NULL;
-		if (execvp(args[0], args) == -1)
+		if (isatty_mode)
+			input = read_input();
+		else
 		{
-			return (-1);
+			input = malloc(sizeof(char) * MAX_LENGTH);
+			if (fgets(input, MAX_LENGTH, stdin) == NULL)
+			break;
 		}
+		input[mystrcspn(input, "\n")] = '\0';
+
+		if (mystrcmp(input, "exit") == 0)
+		{
+			free(input);
+			break;
+		}
+
+		execute_command(input, argv);
+		if (isatty_mode)
+			printf("($) ");
+
+		free(input);
 	}
-	else
-	{
-	if (wait(NULL) == -1)
-	{
-		perror("wait");
-		exit(EXIT_FAILURE);
-	}
-	}
-	return (0);
+
+	if (!isatty_mode)
+		putchar('\n');
+
+	return (EXIT_SUCCESS);
 }
