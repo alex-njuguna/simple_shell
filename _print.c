65f602d4a119967a81f5442bb68d1d5f886ac6bf
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-04-28 13:32:03 +0300

    bug fix

--- _print.c
+++ _print.c
@@ -65,3 +65,87 @@ int _print_error(int errorcode, data_of_program *data)
 	}
 	return (0);
 }
+
+/**
+* mygetline - function to read input
+* @data: program's data
+* Return: count output
+*/
+int mygetline(data_of_program *data)
+{
+	char buff[BUFFER_SIZE] = {'\0'};
+	static char *array_commands[10] = {NULL};
+	static char array_operators[10] = {'\0'};
+	ssize_t bytes_read, i = 0;
+
+	if (!array_commands[0] || (array_operators[0] == '&' && errno != 0) ||
+		(array_operators[0] == '|' && errno == 0))
+	{
+		for (i = 0; array_commands[i]; i++)
+		{
+			free(array_commands[i]);
+			array_commands[i] = NULL;
+		}
+
+		bytes_read = read(data->file_descriptor, &buff, BUFFER_SIZE - 1);
+		if (bytes_read == 0)
+			return (-1);
+
+		i = 0;
+		do {
+			array_commands[i] = str_dup(_strtok(i ? NULL : buff, "\n;"));
+			i = check_logic_ops(array_commands, i, array_operators);
+		} while (array_commands[i++]);
+	}
+	data->input_line = array_commands[0];
+	for (i = 0; array_commands[i]; i++)
+	{
+		array_commands[i] = array_commands[i + 1];
+		array_operators[i] = array_operators[i + 1];
+	}
+
+	return (str_len(data->input_line));
+}
+
+
+/**
+* check_logic_ops - handle && and || operators
+* @array_commands: commands.
+* @i: index
+* @array_operators: logical operators
+* Return: index of the last command
+*/
+int check_logic_ops(char *array_commands[], int i, char array_operators[])
+{
+	char *temp = NULL;
+	int j;
+
+	for (j = 0; array_commands[i] != NULL  && array_commands[i][j]; j++)
+	{
+		if (array_commands[i][j] == '&' && array_commands[i][j + 1] == '&')
+		{
+
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_dup(array_commands[i]);
+			array_commands[i + 1] = str_dup(temp + j + 2);
+			i++;
+			array_operators[i] = '&';
+			free(temp);
+			j = 0;
+		}
+		if (array_commands[i][j] == '|' && array_commands[i][j + 1] == '|')
+		{
+
+			temp = array_commands[i];
+			array_commands[i][j] = '\0';
+			array_commands[i] = str_dup(array_commands[i]);
+			array_commands[i + 1] = str_dup(temp + j + 2);
+			i++;
+			array_operators[i] = '|';
+			free(temp);
+			j = 0;
+		}
+	}
+	return (i);
+}
--- builtins1.c
+++ builtins1.c
@@ -1,6 +1,92 @@
 #include "main.h"
 
 /**
+ * show_env - display env
+ * @data: struct for the program's data
+ * Return: 0
+ */
+int show_env(data_of_program *data)
+{
+	int i;
+	char cpname[50] = {'\0'};
+	char *var_copy = NULL;
+
+	if (data->tokens[1] == NULL)
+		show_environ(data);
+	else
+	{
+		for (i = 0; data->tokens[1][i]; i++)
+		{
+			if (data->tokens[1][i] == '=')
+			{
+				var_copy = str_dup(get_env_var(cpname, data));
+				if (var_copy != NULL)
+					env_set_key(cpname, data->tokens[1] + i + 1, data);
+
+				show_environ(data);
+				if (get_env_var(cpname, data) == NULL)
+				{
+					_print(data->tokens[1]);
+					_print("\n");
+				}
+				else
+				{
+					env_set_key(cpname, var_copy, data);
+					free(var_copy);
+				}
+				return (0);
+			}
+			cpname[i] = data->tokens[1][i];
+		}
+		errno = 2;
+		perror(data->command_name);
+		errno = 127;
+	}
+	return (0);
+}
+
+/**
+ * create_env - set up env
+ * @data: program's data
+ * Return: 0
+ */
+int create_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL || data->tokens[2] == NULL)
+		return (0);
+	if (data->tokens[3] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+
+	env_set_key(data->tokens[1], data->tokens[2], data);
+
+	return (0);
+}
+
+/**
+ * destroy_env - unset env
+ * @data: program's data'
+ * Return: 0
+ */
+int destroy_env(data_of_program *data)
+{
+	if (data->tokens[1] == NULL)
+		return (0);
+	if (data->tokens[2] != NULL)
+	{
+		errno = E2BIG;
+		perror(data->command_name);
+		return (5);
+	}
+	env_delete_key(data->tokens[1], data);
+
+	return (0);
+}
+
+/**
  * builtins1 - functions to find matches and run
  * @data: struct for the program's data
  * Return: func exceecuted or -1
